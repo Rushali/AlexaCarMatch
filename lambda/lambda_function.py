da44dfb75150089d43bbfe9e01f4333be76f149d
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2021-07-22 10:08:27 +0000

    updated lambda/lambda_function.py.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -126,7 +126,8 @@ class SessionEndedRequestHandler(AbstractRequestHandler):
 # *****************************************************************************
 # Resolves catalog value using Entity Resolution
 def resolveEntity(resolvedEntity, slotName):
-    erAuthorityResolution = resolvedEntity[slotName].resolutions.resolutions_per_authority[0]
+    erAuthorityResolution0 = resolvedEntity[slotName].resolutions.resolutions_per_authority[0]
+    erAuthorityResolution1 = resolvedEntity[slotName].resolutions.resolutions_per_authority[1]
     # print(resolvedEntity[slotName].resolutions.resolutions_per_authority[0].status.code)
     # print(resolvedEntity[slotName].resolutions.resolutions_per_authority[1].status.code, 'now in one')
     value = None
