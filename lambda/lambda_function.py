590d494b91f907632de43c072ca42548a616909a
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2021-07-23 09:30:48 +0000

    updated lambda/lambda_function.py.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -65,7 +65,7 @@ class GetRecommendationAPIHandler(AbstractRequestHandler):
                 for car in all_cars:
                     if not math.isnan(float(car['MSRP'])) and int(float(car['MSRP'])) >= 21 and int(float(car['MSRP'])) < 79:
                         filtered_cars.append(car)
-                    print(len(filtered_cars), 'after budget filter')
+                print(len(filtered_cars), 'after budget filter')
             else:
                 print('not filtering for budget')
                 filtered_cars = all_cars
