95de92076c25f212926ceb4de750e646e4c0a154
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2021-07-23 09:46:31 +0000

    updated lambda/lambda_function.py.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -58,15 +58,14 @@ class GetRecommendationAPIHandler(AbstractRequestHandler):
                 for car in all_cars:
                     if not math.isnan(float(car['MSRP'])) and int(float(car['MSRP'])) > 80:
                         filtered_cars.append(car)
-                print(len(filtered_cars))
-                print('not filtering for budget')
+                print(len(filtered_cars), 'after filtering by luxury')
             elif budget == 'midrange':
                 #print('inside midrange')
                 #midrange is 21k to 79k
                 for car in all_cars:
                     if not math.isnan(float(car['MSRP'])) and int(float(car['MSRP'])) >= 21 and int(float(car['MSRP'])) < 79:
                         filtered_cars.append(car)
-                print(len(filtered_cars), 'after budget filter')
+                print(len(filtered_cars), 'after filtering by midrange')
             else:
                 print('not filtering for budget')
                 filtered_cars = all_cars
