34b6fee047ad62254dfcbc20c72116fbcf89f63b
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2021-07-26 11:30:03 +0000

    updated lambda/lambda_function.py.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -154,7 +154,7 @@ class GetRecommendationAPIHandler(AbstractRequestHandler):
         # recommendationResult['brand'] = "BMW"
         # recommendationResult['url'] = "image.com/image"
 
-        response = buildSuccessApiResponse(recommendationResult)
+        response = buildSuccessApiResponse(recommendationResults)
         
         return response
 
